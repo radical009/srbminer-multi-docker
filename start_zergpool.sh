d7ed68503b71141a02e2d68f2d37e233c0e9b13b
radical009 <36770750+radical009@users.noreply.github.com>
2023-10-01 01:26:46 +0900

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $yespower2b --pool $stratum+tcp://power2b.mine.zergpool.com:7445 --wallet $DLs3MGs8jBaQUftCeiTWmU6g7WdMzFdnpq --password $c=DOGE 
+
 ./SRBMiner-MULTI --algorithm yespower2b --pool stratum+tcp://power2b.mine.zergpool.com:7445 --wallet DLs3MGs8jBaQUftCeiTWmU6g7WdMzFdnpq --password c=DOGE --cpu-threads $cpu_threads
