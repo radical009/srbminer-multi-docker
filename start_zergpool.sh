138914af8baf2d64c8609931ba15943ffb8b7bce
radical009 <36770750+radical009@users.noreply.github.com>
2023-10-12 00:26:30 +0900

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
 
-./SRBMiner-MULTI  --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet SHIB:0xb0e6D943369101f3aB76aA60ba0eaf5Fe318548B.radical009 --cpu-threads $cpu_threads
+./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet SHIB:0xb0e6D943369101f3aB76aA60ba0eaf5Fe318548B.radical009 --cpu-threads $cpu_threads
